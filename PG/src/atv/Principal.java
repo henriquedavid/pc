5939eb25e973e6e2c11603f4a3749e91945763a4
Henrique David de Medeiros <henriquedavid@A303-05L.imd.local>
2020-03-10 09:59:07 -0300

    Condição de corrida encontrada.

--- PG/src/atv/Principal.java
+++ PG/src/atv/Principal.java
@@ -4,10 +4,11 @@ public class Principal {
 	public static void main(String[] args) {
 		Counter c = new Counter(1);
 		
-		ThreadModel t1 = new ThreadModel(c);
-		ThreadModel t2 = new ThreadModel(c);
+		for(int a = 0; a < 3; a++) {
 		
-		t1.start();
-		t2.start();
+			ThreadModel t1 = new ThreadModel(c);
+		
+			t1.start();
+		}
 	}
 }
